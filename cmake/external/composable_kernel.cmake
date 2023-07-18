eacdd7079bd548a7933fbf4c8119cbe73dc7dca8
Guangyun Han <guangyunhan@microsoft.com>
2023-07-18 05:40:27 +0000

    Only compile fp32, fp16 and bf16 instances due to excessive long compilation time

--- cmake/external/composable_kernel.cmake
+++ cmake/external/composable_kernel.cmake
@@ -14,6 +14,8 @@ FetchContent_GetProperties(composable_kernel)
 if(NOT composable_kernel_POPULATED)
   FetchContent_Populate(composable_kernel)
   set(BUILD_DEV OFF CACHE BOOL "Disable -Weverything, otherwise, error: 'constexpr' specifier is incompatible with C++98 [-Werror,-Wc++98-compat]" FORCE)
+  # Exclude i8 device gemm instances due to excessive long compilation time and not being used
+  set(DTYPES fp32 fp16 bf16)
   add_subdirectory(${composable_kernel_SOURCE_DIR} ${composable_kernel_BINARY_DIR} EXCLUDE_FROM_ALL)
 
   add_library(onnxruntime_composable_kernel_includes INTERFACE)

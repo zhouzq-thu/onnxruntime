cd3ca3121090bd02a6c8e1532eb9bc63a2ffd181
yf711 <yifanl@microsoft.com>
2023-08-08 00:00:34 -0700

    fix

--- onnxruntime/python/tools/tensorrt/perf/mem_test/run.sh
+++ onnxruntime/python/tools/tensorrt/perf/mem_test/run.sh
@@ -56,7 +56,7 @@ mkdir result
 
 # Run valgrind
 echo $(date +"%Y-%m-%d %H:%M:%S") '[valgrind] Starting memcheck with' ${ONNX_MODEL}
-valgrind --leak-check=full --show-leak-kinds=all --log-file=valgrind.log ${ORT_SOURCE}/build/Linux/Release/onnxruntime_perf_test -e tensorrt -r 1 ${ONNX_MODEL}
+valgrind --leak-check=full --show-leak-kinds=all --num-callers=30 --keep-debuginfo=yes --log-file=valgrind.log ${ORT_SOURCE}/build/Linux/Release/onnxruntime_perf_test -e tensorrt -r 1 ${ONNX_MODEL}
 echo $(date +"%Y-%m-%d %H:%M:%S") '[valgrind] Analyzing valgrind log'
 
 found_leak_summary=false

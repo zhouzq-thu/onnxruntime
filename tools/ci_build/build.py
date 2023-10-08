724c3250ea5c4143d2f513a663e7869d32259b60
Yi Zhang <zhanyi@microsoft.com>
2023-10-08 10:27:09 +0800

    all tests

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1814,9 +1814,9 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
             # models_dir = os.path.join(os.path.dirname(cwd), "models")
             # run_subprocess([os.path.join(cwd, "onnx_test_runner"), "-j 2", models_dir], cwd=cwd, dll_path=dll_path)
         else:
-            # ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", args.test_all_timeout]
-            # run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
-            models_dir = os.path.join(cwd, "models", "zoo")
+            ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", args.test_all_timeout]
+            run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
+            models_dir = os.path.join(cwd, "models")
             print(f"models_dir = {models_dir}")
             run_subprocess([os.path.join(cwd, config, "onnx_test_runner"), "-j 2", models_dir], cwd=cwd, dll_path=dll_path)
 

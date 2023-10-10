c57996f7f81ad7c6bc01bb5355521f9b6ef055b5
Guangyun Han <guangyunhan@microsoft.com>
2023-10-10 08:52:48 +0000

    Backup scripts for convenience

---
+++ convert-llama2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ev
+
+export PYTHONPATH=$HOME/onnxruntime/build_rocm/Release/build/lib
+
+python onnxruntime/python/tools/transformers/models/llama2/llama-v2.py \
+  --model meta-llama/Llama-2-7b-hf \
+  --output-name llama2-7b-hf \
+  --ort \
+  --export \
+  --optimize \
+  --tunable \
+  --tuning

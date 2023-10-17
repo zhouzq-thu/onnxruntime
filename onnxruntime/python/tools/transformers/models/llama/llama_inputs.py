d0addf0d74091327466bbbb3b335ec7abc385ec4
Kunal Vaishnavi <kvaishnavi@microsoft.com>
2023-10-17 00:04:46 +0000

    Add test cases and merged export

--- onnxruntime/python/tools/transformers/models/llama/llama_inputs.py
+++ onnxruntime/python/tools/transformers/models/llama/llama_inputs.py
@@ -3,7 +3,6 @@ from typing import List, Tuple
 import numpy as np
 import torch
 from transformers import LlamaConfig
-import onnxruntime as ort
 
 
 # Get position_ids from attention_mask
@@ -64,6 +63,36 @@ def get_sample_with_past_kv_inputs(
     return inputs
 
 
+# Inputs for all passes with past_key_values
+def get_merged_sample_with_past_kv_inputs(
+    config: LlamaConfig,
+    device: torch.device,
+    batch_size: int,
+    seq_len: int,
+    past_seq_len: int,
+    use_fp16: bool = False,
+    return_dict: bool = False,
+):
+    input_ids = torch.randint(
+        low=0, high=config.vocab_size, size=(batch_size, seq_len), device=device, dtype=torch.int64
+    )
+    attention_mask = torch.ones(batch_size, past_seq_len + seq_len, device=device, dtype=torch.int64)
+    # position_ids is of shape (batch_size, seq_len) for prompt generation, (batch_size, 1) for token generation
+    position_ids = get_position_ids(attention_mask, use_past_kv=(past_seq_len != 0))
+    past_kv = get_sample_past_kv_inputs(config, device, batch_size, past_seq_len, use_fp16)
+
+    if not return_dict:
+        return (input_ids, attention_mask, position_ids, past_kv)
+
+    inputs = {
+        "input_ids": input_ids,
+        "attention_mask": attention_mask,
+        "position_ids": position_ids,
+        "past_key_values": past_kv,
+    }
+    return inputs
+
+
 # Create past_key_values
 def get_sample_past_kv_inputs(
     config: LlamaConfig, device: torch.device, batch_size: int, past_seq_len: int, use_fp16: bool
@@ -105,36 +134,27 @@ def convert_inputs_for_ort(pt_inputs: dict, use_fp16: bool):
 
 
 # Inputs for Microsoft export from https://github.com/microsoft/Llama-2-Onnx
-def get_msft_sample_inputs(config: LlamaConfig, batch_size: int, past_seq_len: int, seq_len: int, use_fp16: bool, share_buffer: bool):
+def get_msft_sample_inputs(config: LlamaConfig, batch_size: int, past_seq_len: int, seq_len: int, use_fp16: bool, split_kv: bool):
     np_dtype = np.float16 if use_fp16 else np.float32
     head_size = config.hidden_size // config.num_attention_heads
     max_seq_len = 2048
 
-    # ort_inputs = {
-    #     "x": np.random.rand(batch_size, seq_len, config.hidden_size).astype(np_dtype),
-    #     "attn_mask": (-10000.0 * np.triu(np.ones((batch_size, max_seq_len, max_seq_len)), k=1)).astype(np_dtype),
-    #     "k_cache": np.random.rand(
-    #         batch_size, config.num_hidden_layers, past_seq_len, config.num_attention_heads, head_size
-    #     ).astype(np_dtype),
-    #     "v_cache": np.random.rand(
-    #         batch_size, config.num_hidden_layers, past_seq_len, config.num_attention_heads, head_size
-    #     ).astype(np_dtype),
-    #     "pos": np.array(past_seq_len, dtype=np.int64),
-    # }
-
-    if share_buffer:
+    if not split_kv:
         ort_inputs = {
-            "x": ort.OrtValue.ortvalue_from_numpy(np.random.rand(batch_size, seq_len, config.hidden_size).astype(np_dtype), "cuda"),
-            "attn_mask": ort.OrtValue.ortvalue_from_numpy((-10000 * np.triu(np.ones((batch_size, max_seq_len, max_seq_len), dtype=np.int32), k=1)).astype(np.int32), "cuda"),
-            "pos": ort.OrtValue.ortvalue_from_numpy(np.array(past_seq_len, dtype=np.int64), "cpu"),
+            "x": np.random.rand(batch_size, seq_len, config.hidden_size).astype(np_dtype),
+            "attn_mask": (-10000.0 * np.triu(np.ones((batch_size, max_seq_len, max_seq_len)), k=1)).astype(np_dtype),
+            "k_cache": np.random.rand(
+                batch_size, config.num_hidden_layers, past_seq_len, config.num_attention_heads, head_size
+            ).astype(np_dtype),
+            "v_cache": np.random.rand(
+                batch_size, config.num_hidden_layers, past_seq_len, config.num_attention_heads, head_size
+            ).astype(np_dtype),
+            "pos": np.array(past_seq_len, dtype=np.int64),
         }
-        for i in range(config.num_hidden_layers):
-            ort_inputs.update({f"k_{i}_cache": ort.OrtValue.ortvalue_from_numpy(np.random.rand(batch_size, config.num_attention_heads, max_seq_len, head_size).astype(np_dtype), "cuda")})
-            ort_inputs.update({f"v_{i}_cache": ort.OrtValue.ortvalue_from_numpy(np.random.rand(batch_size, config.num_attention_heads, max_seq_len, head_size).astype(np_dtype), "cuda")})
     else:
         ort_inputs = {
             "x": np.random.rand(batch_size, seq_len, config.hidden_size).astype(np_dtype),
-            "attn_mask": (-10000 * np.triu(np.ones((batch_size, max_seq_len, max_seq_len), dtype=np.int32), k=1)).astype(np.int32),
+            "attn_mask": (np.triu(np.ones((batch_size, max_seq_len, max_seq_len), dtype=np.int32), k=1) - 1).astype(np.int32),
             "pos": np.array(past_seq_len, dtype=np.int64),
         }
         for i in range(config.num_hidden_layers):

247c63bb6244008d8b65ee9ec79e0b6be645f59c
Yi Zhang <zhanyi@microsoft.com>
2023-11-06 22:05:37 +0800

    temp test

--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1798,19 +1798,6 @@ def run_onnxruntime_tests(args, source_dir, ctest_path, build_dir, configs):
         if len(dll_path_list) > 0:
             dll_path = os.pathsep.join(dll_path_list)
 
-        if not ctest_path and not is_windows():
-            executables = ["onnxruntime_test_all", "onnxruntime_mlas_test"]
-            if args.build_shared_lib:
-                executables.append("onnxruntime_shared_lib_test")
-                executables.append("onnxruntime_global_thread_pools_test")
-                executables.append("onnxruntime_customopregistration_test")
-            for exe in executables:
-                test_output = f"--gtest_output=xml:{cwd}/{exe}.{config}.results.xml"
-                run_subprocess([os.path.join(cwd, exe), test_output], cwd=cwd, dll_path=dll_path)
-        else:
-            ctest_cmd = [ctest_path, "--build-config", config, "--verbose", "--timeout", args.test_all_timeout]
-            run_subprocess(ctest_cmd, cwd=cwd, dll_path=dll_path)
-
         if args.enable_pybind:
             python_path = None
             if args.use_tvm:
